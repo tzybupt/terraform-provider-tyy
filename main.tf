b5a75c92beb3089f5d826471eee3483acdeab71d
root <root@localhost.localdomain>
2024-10-24 14:42:17 +0800

    update

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
+output "random_output" {
+  value = random_string.random
+}
